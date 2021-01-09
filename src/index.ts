f26058a7b32b260ac026b53f24408c5a5c06b045
UNDERBUFFER <ruslanekitsky@gmail.com>
2021-01-09 22:22:34 +0300

    install libs

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
 
 import express from 'express'
+import path from 'path'
+import bodyParser from 'body-parser'
+import { MongoClient } from 'mongodb'
 import { Router } from './router'
+import { MONGODB_CONNECTION_STRING } from './settings'
+
 
 const app = express()
 
+app.set('views', path.join(__dirname, '/views'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+app.use('/static', express.static( path.join(__dirname, '/public') ))
 app.use('/', Router)
 
 app.listen( 3000, (): void => {
--- src/router.ts
+++ src/router.ts
@@ -7,6 +7,7 @@ export const Router: express.Router = express.Router()
 const MainControllerObject: MainController = new MainController()
 const AccurateControllerObject: AccurateController = new AccurateController()
 
+
 Router.get('/', MainControllerObject.get)
 Router.get('/:id', AccurateControllerObject.get)
 Router.post('/', MainControllerObject.post)
